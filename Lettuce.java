199114ca369f3b4ceb71e6bfa0b60522903fa2b5
104012 <104012@broklede.nl>
2014-10-27 15:52:40 +0100

    Lettuce and Wolf

--- Lettuce.java
+++ Lettuce.java
@@ -1,14 +1,24 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
+/**
+ * Lettuce that kills wolves.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 public class Lettuce extends Mover
 {
-	    public void act() 
-		        {
-				        move();
-					        if (foundSomething(Wolf.class)) {
-							            eatSomething(Wolf.class);
-								            } else if (foundSomething(Goat.class)) {
-										                die();
-												        }
-						    }    
+    /**
+     * Act - do whatever the Lettuce wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        move();
+        if (foundSomething(Wolf.class)) {
+            eatSomething(Wolf.class);
+        } else if (foundSomething(Goat.class)) {
+            die();
+        }
+    }    
 }
--- Wolf.java
+++ Wolf.java
@@ -1,19 +1,24 @@
 import greenfoot.*;
 
+/** A wolf that eats goats.
+*
+* @author (your name)
+* @version (a version number or a date)
+*/
 public class Wolf extends Mover
 {
 
-	    public Wolf()
-		        {
-				    }
+    public Wolf()
+    {
+    }
 
-	        public void act()
-			    {
-				            move();
-					            if (foundSomething(Goat.class)) {
-							                eatSomething(Goat.class);
-									        } else if (foundSomething(Lettuce.class)) {
-											            die();
-												            }
-						        }
+    public void act()
+    {
+        move();
+        if (foundSomething(Goat.class)) {
+            eatSomething(Goat.class);
+        } else if (foundSomething(Lettuce.class)) {
+            die();
+        }
+    }
 }
